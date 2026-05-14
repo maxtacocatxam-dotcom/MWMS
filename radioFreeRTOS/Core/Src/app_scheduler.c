25b50851bcdaaf46ff98baedbdeff372edebdaf3
jdapolista <jdapolista@gmail.com>
2026-05-14 10:30:18 -0700

    Commented Controller task

--- radioFreeRTOS/Core/Src/app_scheduler.c
+++ radioFreeRTOS/Core/Src/app_scheduler.c
@@ -1,27 +1,98 @@
-/*
- * app_scheduler.c
+/*****************************************************************************
+ * File Name    : app_scheduler.c
  *
- *  Created on: May 11, 2026
- *      Author: jdapo
- */
-
+ * Description  :
+ * FreeRTOS scheduler/controller task implementation.
+ *
+ * This module is responsible for:
+ *  - Periodic system wake scheduling
+ *  - Event-driven sensor acquisition triggering
+ *  - Coordinating GPS and environmental sensor tasks
+ *
+ * Task Behavior:
+ *  - Wakes periodically every 10 minutes
+ *  - Can also wake immediately from task notification
+ *  - Triggers GPS and sensor acquisition tasks
+ *
+ * Created on : May 11, 2026
+ * Author     : Jeremiah Apolista
+ *****************************************************************************/
 
 #include "app_scheduler.h"
 #include "FreeRTOS.h"
 #include "task.h"
+
 #include "app_GPS.h"
 #include "BME680.h"
 
+/*****************************************************************************/
+/* FreeRTOS Task Handles                                                     */
+/*****************************************************************************/
+
+/*
+ * Handle for the controller/scheduler task.
+ * Used for task notifications and scheduler interaction.
+ */
 TaskHandle_t ControllerTaskHandle;
 
-void vControllerTask(void *pvParameters){
-	const TickType_t xblockTime = pdMS_TO_TICKS( 600000 ); //10 minutes timeout
-	while(1){
-	//Implement a direct task notify with a timeout of 10 minutes to act as our periodic delay
-	//Essentially telling the task, wake on task notify or every 10 minutes
-	ulTaskNotifyTake(pdTRUE, xblockTime);
-	//Received button press or timeout, notify sensor and GPS
-	xTaskNotifyGive(GPSTaskHandle);
-	xTaskNotifyGive(SensorTaskHandle);
-	}
+/*****************************************************************************/
+/* RTOS Controller Task                                                      */
+/*****************************************************************************/
+
+/*
+ ******************************************************************************
+ * Function Name : vControllerTask
+ *
+ * Description:
+ * FreeRTOS controller task responsible for scheduling periodic
+ * telemetry acquisition events.
+ *
+ * Task Flow:
+ *  1. Wait for timeout or external notification
+ *  2. Wake GPS acquisition task
+ *  3. Wake environmental sensor task
+ *
+ * Synchronization:
+ *  - Receives task notifications from switch/button task
+ *  - Sends task notifications to worker tasks
+ *
+ * Parameters:
+ *  pvParameters : Unused
+ *
+ * Returns:
+ *  None
+ *
+ * Notes:
+ *  The controller task acts as the central acquisition scheduler
+ *  for the telemetry system.
+ ******************************************************************************
+ */
+void vControllerTask(void *pvParameters)
+{
+    /*
+     * Periodic telemetry acquisition interval.
+     * System performs automatic acquisition every 10 minutes.
+     */
+    const TickType_t xBlockTime = pdMS_TO_TICKS(600000);
+
+    while (1)
+    {
+        /*
+         * Wait for either:
+         *  - periodic timeout expiration
+         *  - external notification (button press/event)
+         *
+         * pdTRUE clears the notification value on exit.
+         */
+        ulTaskNotifyTake(pdTRUE, xBlockTime);
+
+        // Notify GPS task to begin location acquisition.
+        xTaskNotifyGive(GPSTaskHandle);
+
+        /*
+         * Notify environmental sensor task to begin
+         * environmental data acquisition.
+         */
+        xTaskNotifyGive(SensorTaskHandle);
+    }
 }
